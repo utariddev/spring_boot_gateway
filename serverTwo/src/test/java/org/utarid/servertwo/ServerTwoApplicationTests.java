c3b574592e6034c74ec2599656e5ed1bf4489679
utarid <utarid@protonmail.com>
2023-07-16 11:48:31 +0200

    initial commit for second server

---
+++ serverTwo/src/test/java/org/utarid/servertwo/ServerTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.utarid.servertwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
